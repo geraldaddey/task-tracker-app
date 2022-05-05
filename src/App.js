3f5c3cd830c5cf95533175bc47b1b0a6caf918c6
geraldaddey <geraldaddey@yahoo.com>
2022-05-05 15:42:20 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const name = "Gerald";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Let's Get this Money!</h1>
+      <h2>You deserve it {name}.</h2>
     </div>
   );
 }
