bdb775a706996ccdee2e51e94e33abd9dc908bf8
geraldaddey <geraldaddey@yahoo.com>
2022-05-11 07:21:22 +0000

    eventhandler added to button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,26 @@
+import PropTypes from "prop-types";
 import React from "react";
 
-const Button = () => {
-  return <button className="btn">Add</button>;
+const Button = ({ text, color, onClick }) => {
+  return (
+    <button
+      style={{ backgroundColor: color }}
+      className="btn"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,15 @@ import Button from "./Button";
 const author = "Gerald Addey";
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("click");
+  };
+
   return (
     <header className="header">
       <h1> {title}</h1>
       {/* <h1>by {author} </h1> */}
-      <Button />
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
