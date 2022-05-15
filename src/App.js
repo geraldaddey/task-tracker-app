5c83413341908c8a264226c360b41a5c2a634a8d
geraldaddey <geraldaddey@yahoo.com>
2022-05-15 10:57:51 +0000

    addTask function created, new states set for new task

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ function App() {
     { id: 3, day: "April 29th", text: "road trip", reminder: true },
   ]);
 
+  // Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -29,7 +37,7 @@ function App() {
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
