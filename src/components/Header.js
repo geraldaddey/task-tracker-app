7d4a16371d3c68ac08198aa3f74808b78e214d36
geraldaddey <geraldaddey@yahoo.com>
2022-05-15 10:17:04 +0000

    Styling added to toggle reminder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "./Button";
+import Task from "./Task";
+import Tasks from "./Tasks";
 
 const author = "Gerald Addey";
 
 const Header = ({ title }) => {
   const onClick = () => {
-    console.log("click");
+    console.log("clicked");
   };
 
   return (
